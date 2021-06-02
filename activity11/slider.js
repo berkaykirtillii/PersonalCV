96775c70372e342067761fedea8242bb73447f7a
berkaykirtilii <16050111037@ybu.edu.tr>
2021-06-02 19:01:01 +0300

    activity11slider added

---
+++ activity11/slider.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    $("#slider").bxSlider({
+		auto: true,
+		randomStart: true,
+		speed: 3000,
+		pager: true,
+		pagerType: 'short',
+		pagerSelector : '#pager',
+		minSlides: 1,
+		maxSlides: 1,
+		slideWidth: 250,
+		slideMargin: 10
+	});
+
+	$
+    	
+
+});
